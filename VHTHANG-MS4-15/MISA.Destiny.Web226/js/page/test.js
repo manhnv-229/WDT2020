f273f7ad5c38c49ee0801bf03eb8a3530bbaa8e4
vuthang226 <vuthang226@gmail.com>
2020-12-10 01:00:11 +0700

    js

---
+++ VHTHANG-MS4-15/MISA.Destiny.Web226/js/page/test.js
@@ -0,0 +1,62 @@
+﻿$(document).ready(function () {
+    dialog = $(".m-dialog").dialog({
+        autoOpen: true,
+        width: 700,
+        modal: true,
+    });
+    loadData();
+    
+})
+
+/**
+ * Thực hiện load dữ liệu
+ * Author: Vũ Hữu Thắng
+ * */
+function loadData() {
+    $.ajax({
+        url: 'http://api.manhnv.net/api/customers',
+        method: 'GET',
+    }).done(function (response) {
+        
+        $('#tb-customer tbody').empty();
+        for (var i = 0; i < response.length; i++) {
+            
+            var date = formatDate(response[i].DateOfBirth);
+            var trTable1 = `<tr class="table-row">
+                        <td rowspan="1" colspan="1" style="width: 100px;">
+                            <div class="cell">${response[i].CustomerCode}</div>
+                        </td>
+                        <td rowspan="1" colspan="1" style="width: 143px;">
+                            <div class="cell">${response[i].FullName}</div>
+                        </td>
+                        <td rowspan="1" colspan="1" style="width: 58px;"><div class="cell">${response[i].GenderName}</div></td>
+                        <td rowspan="1" colspan="1" style="width: 100px;"><div class="cell text-align-center">${date}</div></td>
+                        <td rowspan="1" colspan="1" style="width: 72px;"><div class="cell">${response[i].CustomerGroupName}</div></td>
+                        <td rowspan="1" colspan="1" style="width: 119px;"><div class="cell">${response[i].PhoneNumber}</div></td>
+                        <td rowspan="1" colspan="1" style="width: 192px;"><div class="cell">${response[i].Email}</div></td>
+                        <td rowspan="1" colspan="1" style="width: 232px;"><div class="cell">${response[i].Address}</div></td>
+                        <td rowspan="1" colspan="1" class="text-align-right" style="width: 55px;"><div class="cell">${response[i].DebitAmount || ""}</div></td>
+                        <td rowspan="1" colspan="1" style="width: 98px;"><div class="cell">${response[i].MemberCardCode}</div></td>
+                        <td rowspan="1" colspan="1" style="width: 32px;"><div class="cell"></div></td>
+                    </tr>`;
+            
+            $('#tb-customer > tbody:last-child').append(trTable1);
+        }
+        }).fail(function (response) {
+
+        })
+}
+
+    
+
+
+function formatDate(date) {
+    var date = new Date(date);
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    if (day < 10) day = '0' + day;
+    if (month < 10) month = '0' + month;
+    return day + '/' + month + '/' + year;
+}
