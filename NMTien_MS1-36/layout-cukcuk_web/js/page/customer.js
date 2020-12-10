5ce8d523a722fe46dc0c588511ed82b76f45301e
tienhgm <47660593+tienhgm@users.noreply.github.com>
2020-12-10 11:09:42 +0700

    customer.html

---
+++ NMTien_MS1-36/layout-cukcuk_web/js/page/customer.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+/* http://api.manhnv.net/api/customers */
+    //load du lieu 
+    //B1.Goi phuong thuc lay du lieu
+
+    dialog = $(".customer-dialog").dialog({
+        autoOpen: false,
+        height: 400,
+        width: 350,
+        modal: true,
+       
+    });
+    $("#btnAdd").click(function () {
+        dialog.dialog('open');
+    })
+
+    $.ajax({
+        url: 'http://api.manhnv.net/api/customers',
+        method: 'GET',
+        async: false
+
+    }).done(function (response) {
+        console.log(response);
+        //B3. build html va append len UI
+
+        for (var i = 0; i < response.length(); i++) {
+            console.log(response[i]);
+            var trHtml = ``;
+        }
+      
+      
+        debugger;
+    }).fail(function (response) {
+    });
+    debugger;
+    //B2. xu ly du lieu
+   
+
+})
