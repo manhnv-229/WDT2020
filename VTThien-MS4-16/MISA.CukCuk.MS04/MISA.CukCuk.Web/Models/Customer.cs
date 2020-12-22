6cd73948750d68e0fe3a86f27a27284ff95c1fb4
Vũ Thanh Thiên <44043610+VuThanhThien@users.noreply.github.com>
2020-12-22 20:40:15 +0700

    sửa lỗi kết nối db

--- VTThien-MS4-16/MISA.CukCuk.MS04/MISA.CukCuk.Web/Models/Customer.cs
+++ VTThien-MS4-16/MISA.CukCuk.MS04/MISA.CukCuk.Web/Models/Customer.cs
@@ -10,7 +10,7 @@ namespace MISA.CukCuk.Web.Models
         public string CustomerCode { get; set; }
         public string FullName { get; set; }
         public string MemberCardCode { get; set; }
-        public Guid CustomerGroupId { get; set; }
+        public Guid CustomerGroupID { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public int? Gender { get; set; }
         public string Email { get; set; }
@@ -18,47 +18,11 @@ namespace MISA.CukCuk.Web.Models
         public string CompanyName { get; set; }
         public string CompanyTaxCode { get; set; }
         public string Address { get; set; }
-        public int  CustomerId { get; set; }
+        public Guid  CustomerID { get; set; }
        /* public DateTime CreatedDate { get; set; }
         public string ModifiedBy { get; set; }
         public DateTime ModifiedDate { get; set; }*/
-        static List<Customer> _customers = new List<Customer>()
-        {
-            new Customer()
-            {
-                CustomerCode = "KH0001",
-                FullName = "Vũ Thanh Thiên",
-                MemberCardCode = "ABc",
-                Email = "thudan.td@gmail.com",
-                CustomerId=123,
-            },
-            new Customer()
-            {
-                CustomerCode = "KH0002",
-                FullName = "Vũ  Thiên",
-                MemberCardCode = "ABc",
-                Email = "thun.td@gmail.com",
-                CustomerId=1234,
-            },
-            new Customer()
-            {
-                CustomerCode = "KH03",
-                FullName = " Thiên",
-                MemberCardCode = "ABc",
-                Email = "than.td@gmail.com",
-                CustomerId=12345,
-            }
-        };
-        public static List<Customer> Customers {
-            get
-            {
-                return _customers;
-            }
-            set
-            {
-                _customers = value;
-            }
-        }
+        
 
     }
 }
