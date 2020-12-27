22e96738526ed06eea757db1fc3532fa6e6ffab5
ndl9xcdkthd <nguyendanhluu1999@gmail.com>
2020-12-27 21:50:45 +0700

    commit

--- NDLuu_1000/NDL_FrontEnd/JS/page/customer.js
+++ NDLuu_1000/NDL_FrontEnd/JS/page/customer.js
@@ -1,4 +1,5 @@
-﻿$(document).ready(function(){
+﻿
+$(document).ready(function(){
     $.ajax({
         url: 'http://api.manhnv.net/api/customers' ,
         method:'GET',
@@ -10,9 +11,37 @@
     }).fail(function (response){
 
     });
-    
+    dialog = $(".m-dialog").dialog({
+      autoOpen: false,
+      width: 700,
+      modal: true,
+  });
+
+  initEvens();
     
 });
+
+function initEvens() {
+  // Gán các sự kiện:
+  $('#btnAdd').click(function () {
+      dialog.dialog('open');
+  })
+
+  $('#btnCancel').click(function () {
+      dialog.dialog('close');
+  })
+
+  //$('#tbListData').on('dblclick', 'tr', function () {
+  //    alert('á');
+  //})
+
+  $('#tbListData').on('dblclick', 'tr', function () {
+      // load dữ liệu chi tiết:
+
+      // Hiển thị dialog thông tin chi tiết:
+      dialog.dialog('open');
+  })
+}
 /**
  * Thực hiện gán dữ liệu
  * 
